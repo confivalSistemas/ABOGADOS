f53b534c9bfabf8d42468b66bbf113e0707db324
jjmuesesq <jjmuesesq@unal.edu.co>
2019-10-29 16:43:39 -0500

    primer commit rama develop => jalanado datos de campos sobre tabla db_abogados

--- registro_abogados/urls.py
+++ registro_abogados/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'registro_abogados'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('registro/', views.registro, name= 'registro'),
