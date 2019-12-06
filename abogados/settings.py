e79ee383eb2f115abe5470226563a542c7b26185
jjmuesesq <jjmuesesq@unal.edu.co>
2019-12-06 17:55:40 -0500

    probando vistas login de usuario

--- abogados/settings.py
+++ abogados/settings.py
@@ -137,3 +137,5 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
